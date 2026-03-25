7a880cdaba9f20b012e3dd187097c0c740f7bbbf
nskrkeked <nskrkeked@gmail.com>
2026-03-25 17:21:56 +0900

    Clean up script.js by removing RTF content
    Remove RTF formatting and clean up the script.

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-{\rtf1\ansi\ansicpg949\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 ArialMT;}
-{\colortbl;\red255\green255\blue255;\red26\green26\blue26;\red255\green255\blue255;}
-{\*\expandedcolortbl;;\cssrgb\c13333\c13333\c13333;\cssrgb\c100000\c100000\c100000;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs26 \cf2 \cb3 \expnd0\expndtw0\kerning0
 function goPage() \{\cb1 \
 \cb3 window.location.href = "page2.html";\cb1 \
 \cb3 \}}
